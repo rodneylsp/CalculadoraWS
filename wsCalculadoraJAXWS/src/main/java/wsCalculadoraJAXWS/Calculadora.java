a53614edfc9883b1893c2fb4695a147ad2cfe50c
Rodney <rodneylsp@gmail.com>
2015-10-07 23:41:22 -0300

    Subindo projeto

---
+++ wsCalculadoraJAXWS/src/main/java/wsCalculadoraJAXWS/Calculadora.java
@@ -0,0 +1,16 @@
+package wsCalculadoraJAXWS;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style= SOAPBinding.Style.RPC)
+public class Calculadora {
+
+	@WebMethod(operationName = "adicionar")
+	public int add(@WebParam(name="num1") int numero1, @WebParam(name="num2") int numero2){
+		return numero1 + numero2;
+	}
+}
